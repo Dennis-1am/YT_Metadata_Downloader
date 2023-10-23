cbb6f8fbc15f4ae496e9876aaef80927ff007b50
Dennis-1am <dennislam2003@gmail.com>
2023-10-22 22:57:35 -0400

    Project Phase Complete: Proceeding to Refactoring

--- Metadata_Download.py
+++ Metadata_Download.py
@@ -9,20 +9,30 @@ import openpyxl as xl
 import datetime
 from youtube_transcript_api import YouTubeTranscriptApi, formatters
 
+import tkinter
+import customtkinter
+
+import threading # for multithreading
+
+max_num_threads = 10 # max number of threads to run at once
 
 API_Key = os.environ.get('YT_API_KEY')
 
 def getChannelID(channel_url):
     """
-    Given a YouTube channel URL, returns the channel ID.
+    Given a YouTube channel URL, returns the channel ID and channel name.
     """
-    request = requests.get(channel_url)
-    html = bs4.BeautifulSoup(request.text, 'html.parser')
     
-    json_script = html.find("script", {"type": "application/ld+json"})
-    channel_id = json.loads(json_script.text)['itemListElement'][0]['item']['@id'].split('/')[-1]
-    
-    return channel_id
+    try: 
+        request = requests.get(channel_url)
+        html = bs4.BeautifulSoup(request.text, 'html.parser')
+        
+        json_script = html.find("script", {"type": "application/ld+json"})
+        channel_id = json.loads(json_script.text)['itemListElement'][0]['item']['@id'].split('/')[-1]
+        channel_name = json.loads(json_script.text)['itemListElement'][0]['item']['name']
+        return channel_id, channel_name
+    except:
+        return None, None
 
 youtube = googleapiclient.discovery.build(
     "youtube", "v3", developerKey=API_Key)
@@ -47,8 +57,8 @@ def get_playlist_items(playlist_id):
     response = request.execute()
     videos = [response['items'][0]['contentDetails']['videoId']]
     
-    total = response['pageInfo']['totalResults']
-    counts = len(response['items'])
+    total_video = response['pageInfo']['totalResults']
+    counts = len(response['items'][0]['contentDetails']['videoId'])
     
     while 'nextPageToken' in response:
         nextPage = response['nextPageToken']
@@ -97,17 +107,7 @@ def process_video_metadata(video_metadata): # this function needs to be faster
         video_title = metadata['snippet']['title']
         video_description = metadata['snippet']['description']
         video_thumbnail = metadata['snippet']['thumbnails']['default']['url']
-        
-        # srt_formatter = formatters.SRTFormatter()
-        
-        # check if it has subtitles
-        # video_srt_transcript = None
-        # try :
-        #     video_srt_transcript = { srt_formatter.format_transcript(YouTubeTranscriptApi.get_transcript(video_id)) }
-        # except:
-        #     pass
-    
-        
+
         response = {
             'channel_id': channel_id,
             'video_id': video_id,
@@ -119,27 +119,104 @@ def process_video_metadata(video_metadata): # this function needs to be faster
         
         print('Processing video: ' + video_id + "\n\n\n")
         print(response)
-        # convert and write as we go to avoid memory issues with large datasets
-        # convert to dataframe
-        # temp_df = pd.DataFrame([response])
-        # df = pd.concat([df, temp_df], ignore_index=True)
-        # print(df)
+        print("\n\n\n")
         
-    df.to_excel('metadata.xlsx', index=False)
+        temp_df = pd.DataFrame([response])
+        df = pd.concat([df, temp_df], ignore_index=True)
     
     return df
 
-def process():
+def get_transcript(video_ids, pathToDir):
+    
+    srt_formatter = formatters.SRTFormatter()
+    
+    for id in video_ids:
+        video_srt_transcript = None
+        
+        try:
+            video_srt_transcript = srt_formatter.format_transcript(YouTubeTranscriptApi.get_transcript(id))
+        except:
+            pass
+        
+        if(video_srt_transcript == None):
+            continue
+        else:
+
+            file_name = id + '.srt'
+            full_path = os.path.join(pathToDir, file_name)
+            
+            os.makedirs(os.path.dirname(full_path), exist_ok=True)
+            
+            with open(full_path, 'w') as file:
+                # write with the video_id as the file name
+                file.write(video_srt_transcript)
+
+def process(url, path):
     '''
     This function will be called by the main script. It will call all the other functions in this file.
     '''
-    channel_id = getChannelID('https://www.youtube.com/@lehighcollegeofeducation2015')
-    playlist_id = get_playlist(channel_id)
-    videos = get_playlist_items(playlist_id)
-    # print(videos)
-    video_metadata = get_video_metadata(videos)
-    # print(video_metadata)
-    p_data = process_video_metadata(video_metadata)
-    print(p_data)
     
-process()
+    print('Processing channel: ' + url + "\n\n\n")
+    response = getChannelID(url)
+    
+    channel_id = response[0]
+    channel_name = response[1]
+    
+    if(channel_id == None or channel_name == None):
+        return '400 Bad Request'
+    else: 
+        print('Channel ID: ' + channel_id + "\n\n\n")
+        print('Channel Name: ' + channel_name + "\n\n\n")
+        
+        playlist_id = get_playlist(channel_id)
+        videos_ids = get_playlist_items(playlist_id)
+        
+        print('Number of videos: ' + str(len(videos_ids)) + "\n\n\n")
+        
+        video_metadata = get_video_metadata(videos_ids)
+        
+        print('Processing video metadata...\n\n\n')
+        
+        p_dataframe = process_video_metadata(video_metadata) # processed data is in excel format
+
+        # # the directory named after the channel_name
+        directory = f'{channel_name}/'
+        
+        # # write the excel folder to store the channel_name.xlsx file in
+        if path[-1] != '/':
+            path += '/'
+            
+        # # make sure that path has a '/' at the end
+        if not os.path.exists(path + directory):
+            os.makedirs(path + directory)
+        
+        # # write the excel file
+        p_dataframe.to_excel(path + directory + channel_name + '.xlsx', index=False) # write the excel file to the path folder
+        
+        print('Finished writing excel file...\n\n\n')
+        
+        # calculate the number of threads to run
+        num_threads = max(max_num_threads, min(max_num_threads, len(videos_ids)//50 + 1)) # 50 videos per thread)
+        
+        # split the videos_ids into num_threads lists with no overlap
+        videos_ids_split = [videos_ids[i::num_threads] for i in range(num_threads)]
+        
+        # create a list of threads
+        threads = []
+        
+        for i in range(num_threads):
+            # create a thread
+            thread = threading.Thread(target=get_transcript, args=(videos_ids_split[i], path + directory + 'transcripts/'))
+            threads.append(thread)
+            
+            # start the thread
+            thread.start()
+            print('Started thread ' + str(i) + "\n\n\n")
+            
+        # wait for all threads to finish
+        for thread in threads:
+            thread.join() # wait for the thread to finish
+            
+        print('Finished processing transcripts...\n\n\n')
+            
+        return '200 OK' # return a status code of 200 OK
--- YoutubeData_App.py
+++ YoutubeData_App.py
@@ -1,4 +1,52 @@
 import tkinter
 from tkinter import filedialog
-import customtkinter as ctk
+import customtkinter
 import Metadata_Download as md
+
+def submit_button_click():
+   
+    if(url == ""):
+        status_label.configure(text="400 Bad Request", font=("Arial", 8, "bold"), text_color="red")
+        status_label.update()
+        return 0
+    else:
+        s = md.process(url.get(), filedialog.askdirectory())
+        
+        if s == '200 OK':
+            status_label.configure(text=s, font=("Arial", 8, "bold"), text_color="green")
+            status_label.update()
+            return 1
+        else:
+            status_label.configure(text=s, font=("Arial", 8, "bold"), text_color="red")
+            status_label.update()
+            return 0
+
+app = customtkinter.CTk()
+app.geometry("500x200")
+app.maxsize(width=500, height=200)
+app.title("Youtube Downloader")
+
+app.eval('tk::PlaceWindow . center')
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# center the title status_label url link submit_button
+
+title = customtkinter.CTkLabel(app, text="Insert a youtube channel link")
+title.pack(padx=100, pady=10)
+
+status_label = customtkinter.CTkLabel(app, text="Status of download")
+status_label.pack()
+
+url = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, corner_radius=15, textvariable=url)
+link.pack( )
+
+submit_button = customtkinter.CTkButton(app,
+    text = "Submit", 
+    command = lambda: submit_button_click()
+)
+submit_button.pack( pady=10 )
+
+app.mainloop()
